4c27195246b9b7f4a949f1e6cc93f7b4cee80c76
tuansdf <103062682+tuansdf@users.noreply.github.com>
2022-11-10 17:22:47 +0700

    web/feat: add comment

--- apps/web/src/features/post/post-card.tsx
+++ apps/web/src/features/post/post-card.tsx
@@ -4,9 +4,11 @@ import {
   HandThumbUpIcon,
   ShareIcon,
 } from "@heroicons/react/24/outline";
+import { HandThumbUpIcon as HandThumbUpIconSolid } from "@heroicons/react/24/solid";
 import Avatar from "boring-avatars";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { IPost } from "shared-types";
 import useAuthStore from "/src/features/auth/auth.store";
 import Divider from "/src/features/ui/divider";
 import IconButton from "/src/features/ui/icon-button";
@@ -15,22 +17,10 @@ import TextField from "/src/features/ui/text-field";
 dayjs.extend(relativeTime);
 
 interface Props {
-  name: string;
-  time: Date;
-  numLikes: string;
-  numComments: string;
-  numShares: string;
-  body: string;
+  post: IPost;
 }
 
-export default function PostCard({
-  name,
-  time,
-  numLikes,
-  numComments,
-  numShares,
-  body,
-}: Props) {
+export default function PostCard({ post }: Props) {
   const user = useAuthStore((state) => state.user);
 
   return (
@@ -40,12 +30,12 @@ export default function PostCard({
         {/* info */}
         <div className="flex gap-2">
           {/* pic */}
-          <Avatar size={45} name={name} />
+          <Avatar size={45} name={post.user?.firstName} />
           {/* name */}
           <div>
-            <div className="text-sm font-bold">{name}</div>
+            <div className="text-sm font-bold">{post.user?.firstName}</div>
             <div className="text-xs text-gray-500">
-              {dayjs().to(dayjs(time))}
+              {dayjs().to(dayjs(post.created))}
             </div>
           </div>
         </div>
@@ -59,7 +49,7 @@ export default function PostCard({
       {/* body */}
       <div>
         {/* text */}
-        <div className="px-4">{body}</div>
+        <div className="px-4">{post.body}</div>
       </div>
 
       {/* footer */}
@@ -68,14 +58,14 @@ export default function PostCard({
         <div className="mb-3 flex items-center justify-between">
           {/* left */}
           <div className="flex items-center gap-2">
-            <HandThumbUpIcon className="h-5 w-5 rounded-full bg-primary p-1 text-white shadow" />
-            <span className="text-sm text-gray-500">{numLikes}</span>
+            <HandThumbUpIconSolid className="h-5 w-5 rounded-full bg-primary p-1 text-white shadow" />
+            <span className="text-sm text-gray-500">{post.numLikes}</span>
           </div>
 
           {/* right */}
           <div className="flex gap-2 text-sm text-gray-500">
-            <span>{numComments} comments</span>
-            <span>{numShares} shares</span>
+            <span>{post.numComments} comments</span>
+            <span>{post.numShares} shares</span>
           </div>
         </div>
         <Divider />
@@ -95,7 +85,7 @@ export default function PostCard({
           </div>
         </div>
         <Divider />
-        {/* comments */}
+        {/* add comment */}
         <div className="flex items-center gap-2 py-2">
           <div>
             <Avatar size={40} name={user?.firstName} />
@@ -108,6 +98,27 @@ export default function PostCard({
             placeholder="Write a comment..."
           />
         </div>
+
+        {/* comments */}
+        {post.comments?.length ? (
+          <div className="mt-4 mb-2 grid gap-4">
+            {post.comments?.map((comment) => (
+              <div className="flex gap-3">
+                <div>
+                  <Avatar size={40} name={user?.firstName} />
+                </div>
+                <div className="rounded-xl bg-gray-100 px-2 py-1.5 text-sm">
+                  <span className="cursor-pointer font-bold">
+                    {comment.user?.firstName}
+                  </span>
+                  <p className="overflow-hidden text-ellipsis text-gray-900">
+                    {comment.text}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : null}
       </div>
     </div>
   );
--- apps/web/src/features/post/post-list.tsx
+++ apps/web/src/features/post/post-list.tsx
@@ -8,14 +8,7 @@ export default function PostList({ posts }: Props) {
   return (
     <div className="grid w-full gap-4">
       {posts.map((post) => (
-        <PostCard
-          name={post.user?.firstName || ""}
-          time={post.created}
-          body={post.body}
-          numShares={`${post.numShares}`}
-          numComments={`${post.numComments}`}
-          numLikes={`${post.numLikes}`}
-        />
+        <PostCard post={post} />
       ))}
     </div>
   );
